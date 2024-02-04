64609bc9f4b0791dd13d2f93562cb04f00445e56
niveshsaiteja <107808288+niveshsaiteja@users.noreply.github.com>
2024-02-04 18:07:55 +0530

    updated navbar

--- main.js
+++ main.js
@@ -2,23 +2,19 @@ console.log("working")
 
 let burgerAction = document.getElementById("burgerAction");
 let crossAction = document.getElementById("crossAction");
-let companyBtn = document.getElementById("companyBtn");
+let navBar2 = document.getElementById("navBar2");
 let mainNav= document.getElementById("mainNav");
-let navigationBtns = document.getElementById("navigationBtns");
 
 burgerAction.onclick = function(){
     burgerAction.classList.add("no-display");
     crossAction.classList.remove("no-display");
 
-    companyBtn.style.display="block"
-    navigationBtns.style.display="block";
-    mainNav.classList.add("main-nav-background");
+    navBar2.style.display="flex"
 }
 
 crossAction.onclick = function(){
     burgerAction.classList.remove("no-display");
     crossAction.classList.add("no-display");
-    companyBtn.style.display="none"
-    navigationBtns.style.display="none";
-    mainNav.classList.remove("main-nav-background");
+    navBar2.style.display="none"
+   
 }
