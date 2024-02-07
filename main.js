fc1e9945b85b0d512e36bc0722b7ac74af0d6ab8
niveshsaiteja <107808288+niveshsaiteja@users.noreply.github.com>
2024-02-07 13:07:12 +0530

    swipper added

--- main.js
+++ main.js
@@ -71,3 +71,21 @@ let returnId3 = setInterval(()=>{
         closeInterval(returnId3);
     }
 },10)
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
